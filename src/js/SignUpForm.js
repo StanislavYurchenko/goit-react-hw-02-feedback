1596f38192af63f9ab425fdf879d2f5491500125
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-19 17:24:28 +0300

    first

--- src/js/SignUpForm.js
+++ src/js/SignUpForm.js
@@ -226,7 +226,7 @@ class SignUpForm extends Component {
               id={ this.id.genderMale }
               onChange={ this.changeHandler }>
             </input>
-            <Label htmlFor={this.id.genderMale}>{ this.id.genderMale }</Label>
+            <Label htmlFor={this.id.genderMale}>Male</Label>
             
             <input
               type="radio"
@@ -236,7 +236,7 @@ class SignUpForm extends Component {
               id={ this.id.genderFemale }
               onChange={ this.changeHandler }>
             </input>
-            <Label htmlFor={ this.id.genderFemale }>{ this.id.genderFemale }</Label>
+            <Label htmlFor={ this.id.genderFemale }>Female</Label>
           </div>
           
         </Box>
