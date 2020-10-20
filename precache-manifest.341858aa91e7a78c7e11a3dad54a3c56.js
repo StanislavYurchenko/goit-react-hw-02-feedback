99f7139ee29118b59a039ca9a7152a5344750ed3
Stas Yurchenko <yurchenko.stanislav@ukr.net>
2020-10-20 18:56:10 +0300

    Updates

---
+++ precache-manifest.341858aa91e7a78c7e11a3dad54a3c56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f300fe2448100fc45184c72bdcc34162",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "889b12a9e47464b4d9f7",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c1d59827.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/goit-react-hw-02-feedback/static/js/2.c1d59827.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115ee50affa20cf1cc2a",
+    "url": "/goit-react-hw-02-feedback/static/js/main.503a21a3.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.ff13a88926c61ac677c6ca85953c16d3.js"
+  "/goit-react-hw-02-feedback/precache-manifest.341858aa91e7a78c7e11a3dad54a3c56.js"
 );
 
 self.addEventListener('message', (event) => {
